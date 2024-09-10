1325d3d68fdb77150c4d1b60859e30737773fcfc
Sivaroopan Nirhoshan <nirhosh98@gmail.com>
2024-09-10 10:47:57 +0530

    Remove authentication

--- app/controllers/api/init.go
+++ app/controllers/api/init.go
@@ -8,7 +8,7 @@ import (
 func init() {
 	revel.InterceptFunc(authentication.Authenticate, revel.BEFORE, &UserController{})
 	revel.InterceptFunc(authentication.Authenticate, revel.BEFORE, &PublisherController{})
-	revel.InterceptFunc(authentication.Authenticate, revel.BEFORE, &EntityEditController{})
+	// revel.InterceptFunc(authentication.Authenticate, revel.BEFORE, &EntityEditController{})
 	revel.InterceptFunc(authentication.Authenticate, revel.BEFORE, &TokenValidationController{})
 	revel.InterceptFunc(authentication.Authenticate, revel.BEFORE, &FileUploadController{})
 }
