44b1a094be38a950beb8ea8ab620be59bada5708
Sivaroopan Nirhoshan <nirhosh98@gmail.com>
2024-09-11 14:18:59 +0530

    Add logs to get token

--- app/services/authentication/get_token_string.go
+++ app/services/authentication/get_token_string.go
@@ -3,17 +3,20 @@ package authentication
 import (
 	"GIG/app/constants/error_messages"
 	"errors"
-	"github.com/revel/revel"
+	"log"
 	"strings"
+
+	"github.com/revel/revel"
 )
 
 func getTokenString(header *revel.RevelHeader, headerName string) (tokenString string, err error) {
 	authHeader := header.Get(headerName)
+	log.Println(authHeader)
 	if authHeader == "" {
 		return "", errors.New(error_messages.AuthHeaderNotFound)
 	}
-
 	tokenSlice := strings.Split(authHeader, " ")
+	log.Println(tokenSlice)
 	if len(tokenSlice) != 2 {
 		return "", errors.New(error_messages.InvalidTokenFormat)
 	}
